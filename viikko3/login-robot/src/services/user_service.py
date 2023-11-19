61f5ad90fa149de29bf797cfa8ceb1d758373f61
MiikaMatias <100348027+MiikaMatias@users.noreply.github.com>
2023-11-19 05:40:34 +0200

    add service

---
+++ viikko3/login-robot/src/services/user_service.py
@@ -0,0 +1,50 @@
+import re
+from entities.user import User
+
+
+class UserInputError(Exception):
+    pass
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self._user_repository = user_repository
+
+    def check_credentials(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        user = self._user_repository.find_by_username(username)
+
+        if not user or user.password != password:
+            raise AuthenticationError("Invalid username or password")
+
+        return user
+
+    def create_user(self, username, password):
+        self.validate(username, password)
+
+        user = self._user_repository.create(
+            User(username, password)
+        )
+
+        return user
+
+    def validate(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+        if self._user_repository.find_by_username(username) != None:
+            raise AuthenticationError("Username taken")
+        if not re.match("^[a-z]+$", username):
+            raise UserInputError("Shit letters try again bozo")
+        if len(username) < 3:
+            raise UserInputError("Shit length bozo")
+        if len(password) < 3:
+            raise UserInputError("Shit pass try again bozo")
+        if re.match("^[a-z]+$", password):
+            raise UserInputError("Only letters in pass try again bozo")
+        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
